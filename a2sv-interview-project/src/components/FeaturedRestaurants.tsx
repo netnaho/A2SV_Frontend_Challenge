2f10289e8c136621cbfc7c057c3a468ade8a8ae3
netnaho <87896136+netnaho@users.noreply.github.com>
2025-01-28 11:41:55 +0300

    reduce FeaturedRestaurants array length from 9 to 8

--- a2sv-interview-project/src/components/FeaturedRestaurants.tsx
+++ a2sv-interview-project/src/components/FeaturedRestaurants.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SingleRestaurant from "./SingleRestaurant";
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const FeaturedRestaurants = () => {
   return (
